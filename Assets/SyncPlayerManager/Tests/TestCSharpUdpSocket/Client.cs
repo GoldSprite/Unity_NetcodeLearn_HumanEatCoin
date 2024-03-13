d33e4f2c300a7b1541e19e32173d6f57231450c1
shikeik <30600841+shikeik@users.noreply.github.com>
2024-03-13 23:25:42 +0800

    #### 24.3.13.1-尝试云服多客户端通信 1. 多客户端通信无问题

--- Assets/SyncPlayerManager/Tests/TestCSharpUdpSocket/Client.cs
+++ Assets/SyncPlayerManager/Tests/TestCSharpUdpSocket/Client.cs
@@ -14,6 +14,14 @@ namespace Assets.SyncPlayerManager.Tests.TestCSharpUdpSocket {
         private Thread t2;
         private Thread t;
 
+        public string ip = "192.168.1.105";
+        public int port = 34002;
+        public IPEndPoint localPoint;
+        [Space]
+        public string remoteIp = "162.14.68.248";
+        public int remotePort = 34002;
+        public IPEndPoint remotePoint;
+
 
         private void OnDestroy()
         {
@@ -42,8 +50,11 @@ namespace Assets.SyncPlayerManager.Tests.TestCSharpUdpSocket {
         [ContextMenu("Start")]
         public void Main()
         {
+            localPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+            remotePoint = new IPEndPoint(IPAddress.Parse(remoteIp), remotePort);
+
             client = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            client.Bind(new IPEndPoint(IPAddress.Parse("192.168.1.105"), 34002));
+            client.Bind(localPoint);
             //t = new Thread(sendMsg);
             //t.Start();
             t2 = new Thread(ReciveMsg);
@@ -52,37 +63,15 @@ namespace Assets.SyncPlayerManager.Tests.TestCSharpUdpSocket {
         }
 
 
-        static EndPoint point = new IPEndPoint(IPAddress.Parse("162.14.68.248"), 34001);
         [ContextMenu("SendMsg2")]
         public void sendMsg2()
         {
             Debug.Log("发送");
             string msg = "和雕塑高富帅";
-            client.SendTo(Encoding.UTF8.GetBytes(msg), point);
+            client.SendTo(Encoding.UTF8.GetBytes(msg), remotePoint);
         }
 
 
-        /// <summary>
-        /// 向特定ip的主机的端口发送数据报
-        /// </summary>
-        static void sendMsg()
-        {
-            try {
-                while (true) {
-                    Debug.Log("发送");
-                    string msg = "和雕塑高富帅";
-                    client.SendTo(Encoding.UTF8.GetBytes(msg), point);
-
-                    Debug.Log("等两秒");
-                    Thread.Sleep(2000);
-                }
-            }
-            catch (Exception e) {
-                Debug.LogWarning("线程已被终止.");
-            }
-
-
-        }
         /// <summary>
         /// 接收发送给本机ip对应端口号的数据报
         /// </summary>
